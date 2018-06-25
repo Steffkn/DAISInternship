7d5233406d5f4d321e6e2b6e82bbc7fd79fd05a9
steffkn <steff.kn@abv.bg>
2018-06-25 10:56:59 +0300

    Refactoring Extracting methods from NeuralNetworkXORTake2.cs

--- NeuralNetworks/NeuralNetworkXOR/NeuralNetworkXORTake2/NeuralNetworkXORTake2.cs
+++ NeuralNetworks/NeuralNetworkXOR/NeuralNetworkXORTake2/NeuralNetworkXORTake2.cs
@@ -11,12 +11,9 @@ namespace NeuralNetworkXORTake2
 
     public class NeuralNetworkXORTake2
     {
-        // we can add seed later
-        private static Random Rand = new Random();
-
         static void Main()
         {
-            string fileName = @".\inputs\AND.txt";
+            string fileName = @"inputs\AND.txt";
             const int iterations = 10000;
 
             try
@@ -60,7 +57,7 @@ namespace NeuralNetworkXORTake2
             {
                 Console.WriteLine("File reading exception! \n{0}", ex.Message);
                 Console.WriteLine("Loading random weights...");
-                LoadRandomWeights(numberOfinputNeurons, numberOfoutputNeurons, numberOfhiddenNeurons, weightRows, weights);
+                Mind.LoadRandomWeights(numberOfinputNeurons, numberOfoutputNeurons, numberOfhiddenNeurons, weightRows, weights);
             }
 
             for (int x = 0; x < weights.GetLength(0); x++)
@@ -74,46 +71,60 @@ namespace NeuralNetworkXORTake2
                 Mind.Neurons.Add(new Neuron());
             }
 
+            while (true)
+            {
+                Train(fileName, iterations, weights, deltaWeights);
+                Test(weights, deltaWeights);
+            }
+        }
+
+        public static void Test(double[][] weights, double[][] deltaWeights)
+        {
+            var input = new int[Mind.Inputs.GetLength(1)];
+
+            for (int i = 0; i < Mind.Inputs.GetLength(0); i++)
+            {
+                Console.WriteLine("Test the network");
+                for (int x = 0; x < input.Length; x++)
+                {
+                    Console.Write($"x{x} = ");
+                    string inputString = Console.ReadLine();
+                    input[x] = int.Parse(inputString == string.Empty ? "0" : inputString);
+                }
+
+                // forward propagation
+                Mind.ForwardPropagation(weights, input);
+
+                // simple backpropagation
+                Mind.BackwardPropagation(Mind.Outputs[i], weights, deltaWeights, input);
+
+                Console.WriteLine(" ===== Error: {0:F6}", Mind.Neurons[0].DeltaErrors);
+                Console.WriteLine(" ===== Prediction: {0:F6}", Mind.Neurons[0].NeuronSum);
+            }
+        }
+
+        public static void Train(string fileName, int iterations, double[][] weights, double[][] deltaWeights)
+        {
             for (int z = 0; z <= iterations; z++)
             {
                 var input = new int[Mind.Inputs.GetLength(1)];
 
                 for (int i = 0; i < Mind.Inputs.GetLength(0); i++)
                 {
-                    if (z == iterations)
+                    for (int x = 0; x < input.Length; x++)
                     {
-                        Console.WriteLine("Test the network");
-                        for (int x = 0; x < input.Length; x++)
-                        {
-                            Console.Write($"x{x} = ");
-                            string inputString = Console.ReadLine();
-                            input[x] = int.Parse(inputString == string.Empty ? "0" : inputString);
-                        }
-                    }
-                    else
-                    {
-                        for (int x = 0; x < input.Length; x++)
-                        {
-                            input[x] = Mind.Inputs[i, x];
-                        }
+                        input[x] = Mind.Inputs[i, x];
                     }
 
                     // forward propagation
-                    ForwardPropagation(weights, input);
+                    Mind.ForwardPropagation(weights, input);
 
                     // simple backpropagation
-                    BackwardPropagation(Mind.Outputs[i], weights, deltaWeights, input);
-
-                    if (z == iterations)
-                    {
-                        Console.WriteLine(" ===== Error: {0:F6}", Mind.Neurons[0].DeltaErrors);
-                        Console.WriteLine(" ===== Prediction: {0:F6}", Mind.Neurons[0].NeuronSum);
-
-                        WriteToFile(fileName, weights);
-                        z = 0;
-                    }
+                    Mind.BackwardPropagation(Mind.Outputs[i], weights, deltaWeights, input);
                 }
             }
+
+            WriteToFile(fileName, weights);
         }
 
         private static void LoadWeights(string fileName, double[][] weights)
@@ -141,12 +152,11 @@ namespace NeuralNetworkXORTake2
         private static int[,] LoadInput(string[] fileInput)
         {
             int line = 0;
-            int[,] result;
             string[] lineText = fileInput[line].Split(new char[] { 'x' }, StringSplitOptions.RemoveEmptyEntries);
             int inputLenghtX = int.Parse(lineText[0]);
             int inputLenghtY = int.Parse(lineText[1]);
 
-            result = new int[inputLenghtX, inputLenghtY];
+            int[,] result = new int[inputLenghtX, inputLenghtY];
 
             for (int i = 0; i < inputLenghtX; i++)
             {
@@ -183,95 +193,7 @@ namespace NeuralNetworkXORTake2
             return result;
         }
 
-        private static void LoadRandomWeights(int inputNeurons, int outputNeurons, int hiddenNeurons, int weightRows, double[][] weights)
-        {
-            // input weights
-            for (int x = 0; x < inputNeurons; x++)
-            {
-                weights[x] = new double[hiddenNeurons];
 
-                for (int y = 0; y < hiddenNeurons; y++)
-                {
-                    System.Threading.Thread.Sleep(111);
-                    weights[x][y] = Rand.NextDouble();
-                }
-            }
-
-            // last (1) hidden layer weights
-            for (int x = inputNeurons; x < weightRows; x++)
-            {
-                weights[x] = new double[outputNeurons];
-
-                for (int y = 0; y < outputNeurons; y++)
-                {
-                    System.Threading.Thread.Sleep(111);
-                    weights[x][y] = Rand.NextDouble();
-                }
-            }
-        }
-
-        private static void BackwardPropagation(int target, double[][] weights, double[][] deltaWeights, int[] input)
-        {
-            // output errors
-            Mind.Neurons[0].TotalErrors = target - Mind.Neurons[0].NeuronSum;
-            Mind.Neurons[0].DeltaErrors = Mind.Neurons[0].TotalErrors * Mind.DerivativeTanH(Mind.Neurons[0].NeuronSum);
-
-            // hidden errors
-            for (int error = 1; error < Mind.Neurons.Count; error++)
-            {
-                Mind.Neurons[error].TotalErrors = Mind.Neurons[0].TotalErrors * weights[error + 1][0] * Mind.DerivativeTanH(Mind.Neurons[error].NeuronSum);
-                Mind.Neurons[error].DeltaErrors = Mind.Neurons[error].TotalErrors * Mind.DerivativeTanH(Mind.Neurons[error].NeuronSum);
-            }
-
-            // number of neurons till output 2
-            for (int x = 0; x < 2; x++)
-            {
-                for (int y = 0; y < deltaWeights[x].Length; y++)
-                {
-                    deltaWeights[x][y] = input[x] * Mind.Neurons[y + 1].DeltaErrors;
-                }
-            }
-
-            deltaWeights[2][0] = Mind.Neurons[1].NeuronSum * Mind.Neurons[0].DeltaErrors;
-            deltaWeights[3][0] = Mind.Neurons[2].NeuronSum * Mind.Neurons[0].DeltaErrors;
-            deltaWeights[4][0] = Mind.Neurons[3].NeuronSum * Mind.Neurons[0].DeltaErrors;
-
-            CalculateNewWeights(weights, deltaWeights);
-        }
-
-        private static void CalculateNewWeights(double[][] weights, double[][] deltaWeights)
-        {
-            for (int x = 0; x < weights.GetLength(0); x++)
-            {
-                for (int y = 0; y < weights[x].GetLength(0); y++)
-                {
-                    weights[x][y] += deltaWeights[x][y] * Mind.Learning_Rate;
-                }
-            }
-        }
-
-        private static void ForwardPropagation(double[][] weights, int[] input)
-        {
-            for (int net = 1; net < Mind.Neurons.Count; net++)
-            {
-                Mind.Neurons[net].NetSum = 0;
-                for (int index = 0; index < input.Length; index++)
-                {
-                    Mind.Neurons[net].NetSum += input[index] * weights[index][net - 1];
-                }
-
-                Mind.Neurons[net].NeuronSum = Mind.TanH(Mind.Neurons[net].NetSum);
-            }
-
-            // output neuron is index 0
-            Mind.Neurons[0].NetSum = 0;
-            for (int neuronIndex = 1; neuronIndex < Mind.Neurons.Count; neuronIndex++)
-            {
-                Mind.Neurons[0].NetSum += Mind.Neurons[neuronIndex].NeuronSum * weights[neuronIndex + 1][0];
-            }
-
-            Mind.Neurons[0].NeuronSum = Mind.TanH(Mind.Neurons[0].NetSum);
-        }
 
 
         public static void WriteToFile(string fileName, double[][] weights)
